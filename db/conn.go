6915f99e8b5bf77e0c282709c6b5767adbe61966
GuilhermeFujita <fujitaguilherme@gmail.com>
2025-03-23 10:47:11 -0300

    Using env variables

--- db/conn.go
+++ db/conn.go
@@ -3,19 +3,31 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "<host>"
-	port     = 5432
-	user     = "<db_user>"
-	password = "<db_password>"
-	dbname   = "<db_name>"
-)
 
 func ConnectDB() (*sql.DB, error) {
+	if err := godotenv.Load(".env"); err != nil {
+		panic("Error loading .env file")
+	}
+
+	portStr := os.Getenv("DB_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		port = 5432
+		fmt.Print("Failed to use env variable port. Using default port 5432")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.25.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
 
